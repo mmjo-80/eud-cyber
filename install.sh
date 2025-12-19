90ec734683c5c639964e542e5ce1b7f9178c42ab
mmjo-80 <mmjo@mercantec.dk>
2025-12-19 18:56:43 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 set -e
 
+
 GUACVM="./GUACVM/GUACVM_installer.sh"
 OPENVSWITCH="./open-vswitch.sh"
 VULNSRV01="./VULNSRV01/VULNSRV01_installer.sh"
+OPNSENSE="./OPENSENSE/OPNSENSE_installer.sk"
+
 echo "Starting Open-vswitch installation and configuration"
 bash "$OPENVSWITCH"
+echo "Starting Opnsense VM creation..."
+bash "$OPNSENSE"
 echo "Starting Guacamole VM creation..."
 bash "$GUACVM"
 echo "Starting Vuln-server01 VM creation..."
 bash "$VULNSRV01"
+
+
 
 
