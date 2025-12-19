3ea6ccd9eeb170e7de46c2334351ae77d438a48b
mmjo-80 <mmjo@mercantec.dk>
2025-12-19 17:27:53 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@ set -e
 
 GUACVM="./GUACVM/GUACVM_installer.sh"
 OPENVSWITCH="./open-vswitch.sh"
-VULN-SRV01="./VULN-SRV01/VULN-SRV01_installer.sh"
+VULNSRV01="./VULNSRV01/VULNSRV01_installer.sh"
 echo "Starting Open-vswitch installation and configuration"
 bash "$OPENVSWITCH"
 echo "Starting Guacamole VM creation..."
 bash "$GUACVM"
 echo "Starting Vuln-server01 VM creation..."
-bash "$VULN-SRV01"
+bash "$VULNSRV01"
 
 
