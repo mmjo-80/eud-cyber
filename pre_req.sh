e047d0e9cc65a3cb3f7e06135bbd2f38e037546a
mmjo-80 <mmjo@mercantec.dk>
2025-12-22 11:29:42 +0100

    Update pre_req.sh

--- pre_req.sh
+++ pre_req.sh
@@ -43,7 +43,7 @@ chmod 755 "$SNIPPETS_DIR"
 echo "✔ Proxmox snippets setup complete"
 
 #check if packages is installed is not install them
-PACKAGES=(wget bzip2 openvswitch-switch ifupdown2)
+PACKAGES=(wget bzip2 openvswitch-switch ifupdown2 ipcalc)
 
 for pkg in "${PACKAGES[@]}"; do
   if ! dpkg -s "$pkg" >/dev/null 2>&1; then
